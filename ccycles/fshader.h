63de4598bc488a509eccdf49e4e4ba7599c09dd2
Nathan Letwory <nathan@mcneel.com>
2017-01-30 14:52:52 +0200

    Add sources for vertex and fragment shaders.

---
+++ ccycles/fshader.h
@@ -0,0 +1,15 @@
+		"#version 330                                 \n"
+
+		"uniform sampler2D tex;                       \n"
+		"uniform vec2 viewport_size;                  \n"
+
+		"out vec4 Color;                              \n"
+
+		"void main()                                  \n"
+		"{                                            \n"
+		"  vec2 tc = gl_FragCoord.xy / viewport_size; \n"
+
+		"  vec4 px = texture(tex, tc);                \n"
+		"  Color = vec4(px.rgb, 1.0);                 \n"
+		//"  Color = vec4(0.2, 0.5, 0.8, 1.0);"
+		"}                                            \n";
