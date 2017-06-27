a43980ecbbdc83d435a5b523a00d67ef6c245e0e
Nathan Letwory <nathan@mcneel.com>
2017-06-27 17:22:10 +0300

    Clean up split kernel build script.

--- build_split_cubins.bat
+++ build_split_cubins.bat
@@ -22,19 +22,11 @@ SET shadermodel=sm_%shadermodelnum%
 
 IF [%shadermodelnum%]==[] (
 	FOR %%s IN ("sm_20", "sm_21") DO (
-		"%nvcc%" -arch=%%s -m64 --cubin %cyclesroot%/cycles/src/kernel/kernels/cuda/kernel.cu -o %cyclesout%/lib/kernel_%%s.cubin --ptxas-options="-v" -D__KERNEL_CUDA_VERSION__=%cudaversion% --use_fast_math -I%cyclesroot%/cycles/src -DCCL_NAMESPACE_BEGIN= -DCCL_NAMESPACE_END= -DNVCC
-	)
-	FOR %%s IN ("sm_20", "sm_21") DO (
 		"%nvcc%" -arch=%%s -m64 --cubin %cyclesroot%/cycles/src/kernel/kernels/cuda/kernel_split.cu -o %cyclesout%/lib/kernel_split_%%s.cubin --ptxas-options="-v" -D__KERNEL_CUDA_VERSION__=%cudaversion% --use_fast_math -I%cyclesroot%/cycles/src -DCCL_NAMESPACE_BEGIN= -DCCL_NAMESPACE_END= -DNVCC
-	)
-	FOR %%s IN ("sm_30", "sm_35", "sm_50", "sm_52", "sm_60", "sm_61") DO (
-		"%nvcc%" -arch=%%s -m64 --cubin %cyclesroot%/cycles/src/kernel/kernels/cuda/kernel.cu -o %cyclesout%/lib/kernel_%%s.cubin --ptxas-options="-v" -D__KERNEL_CUDA_VERSION__=%cudaversion75% --use_fast_math -I%cyclesroot%/cycles/src -DCCL_NAMESPACE_BEGIN= -DCCL_NAMESPACE_END= -DNVCC
 	)
 	FOR %%s IN ("sm_30", "sm_35", "sm_50", "sm_52", "sm_60", "sm_61") DO (
 		"%nvcc%" -arch=%%s -m64 --cubin %cyclesroot%/cycles/src/kernel/kernels/cuda/kernel_split.cu -o %cyclesout%/lib/kernel_split_%%s.cubin --ptxas-options="-v" -D__KERNEL_CUDA_VERSION__=%cudaversion75% --use_fast_math -I%cyclesroot%/cycles/src -DCCL_NAMESPACE_BEGIN= -DCCL_NAMESPACE_END= -DNVCC
 	)
 ) ELSE (
-	"%nvcc%" -arch=%shadermodel% -m64 --cubin %cyclesroot%/cycles/src/kernel/kernels/cuda/kernel.cu -o %cyclesout%/lib/kernel_%shadermodel%.cubin --ptxas-options="-v" -D__KERNEL_CUDA_VERSION__=%cudaversion75% --use_fast_math -I%cyclesroot%/cycles/src -DCCL_NAMESPACE_BEGIN= -DCCL_NAMESPACE_END= -DNVCC
-	"%nvcc%" -arch=%shadermodel% -m64 --cubin %cyclesroot%/cycles/src/kernel/kernels/cuda/filter.cu -o %cyclesout%/lib/filter_%shadermodel%.cubin --ptxas-options="-v" -D__KERNEL_CUDA_VERSION__=%cudaversion75% --use_fast_math -I%cyclesroot%/cycles/src -DCCL_NAMESPACE_BEGIN= -DCCL_NAMESPACE_END= -DNVCC
 	"%nvcc%" -arch=%shadermodel% -m64 --cubin %cyclesroot%/cycles/src/kernel/kernels/cuda/kernel_split.cu -o %cyclesout%/lib/kernel_split_%shadermodel%.cubin --ptxas-options="-v" -D__KERNEL_CUDA_VERSION__=%cudaversion75% --use_fast_math -I%cyclesroot%/cycles/src -DCCL_NAMESPACE_BEGIN= -DCCL_NAMESPACE_END= -DNVCC
 )
