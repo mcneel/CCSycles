ebbbdbc36b39ce9fbf7a31997f02af12934519a8
Nathan Letwory <nathan@mcneel.com>
2017-09-18 15:16:49 +0300

    Property was constructed wrong, leading to compile error.

--- csycles/Transform.cs
+++ csycles/Transform.cs
@@ -198,7 +198,7 @@ namespace ccl
 		/// <summary>
 		/// Conversion matrix for rhino-cycles camera
 		/// </summary>
-		static readonly public Transform RhinoToCyclesCam { get; private set; } = new Transform(
+		static public Transform RhinoToCyclesCam { get; } = new Transform(
 			1.0f, 0.0f, 0.0f, 0.0f,
 			0.0f, 1.0f, 0.0f, 0.0f,
 			0.0f, 0.0f, -1.0f, 0.0f,
