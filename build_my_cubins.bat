2a572cef52c52cfa213cc551ec81513e576bdff1
Nathan Letwory <nathan@mcneel.com>
2017-10-26 11:41:13 +0300

    Disable kernel parts not used for RhinoCycles.

--- build_my_cubins.bat
+++ build_my_cubins.bat
@@ -1,7 +1,6 @@
 call build_filter_cubins.bat
-call build_kernel_cubins.bat rhino 52 
+REM call build_kernel_cubins.bat rhino 52 
 call build_kernel_cubins.bat rhino 60
-
 call build_kernel_cubins.bat rhino 61
 
 copy D:\Dev\Rhino\rhino\big_libs\RhinoCycles\lib\*.cubin  D:\Dev\Rhino\rhino\src4\bin\Debug\Plug-ins\RhinoCycles\lib
